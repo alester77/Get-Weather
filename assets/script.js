7d14d32571c3ab7219350f6ae494ad1ce9dc59e1
alester77 <113203488+alester77@users.noreply.github.com>
2023-08-01 14:34:55 -0600

    linked weather api successfully

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,15 @@
-var apiUrl =  "https://api.openweathermap.org/data/2.5/weather?q=" +cityName+ "&units=imperial&appid=f68e4f6cffb069c26fec9c4450b3ac2d";
-var citySearch = 
+var apiKey = "f68e4f6cffb069c26fec9c4450b3ac2d";
+var citySearch = 'Denver';
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ citySearch +"&appid=" +apiKey;
+
+// document.querySelector("city-selection");
+var requestUrl = apiUrl
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    // data.coord.lat and data.coord.lon will contain the latitude and longitude
+  });
